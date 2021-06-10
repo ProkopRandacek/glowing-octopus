31d95068691e8f3c8d254040feb2a8170093ee2b
Prokop Randáček <prokop@randacek.dev>
2021-06-10 23:47:22 +0200

    walking fix

--- slave_1.0.0/control.lua
+++ slave_1.0.0/control.lua
@@ -1,10 +1,41 @@
 require("util")
-require("helper")
 
 local walking_state = {walking = false} --  what direction we are walking and if were walking
 local walking_to = {} -- the position that were walking to
 local p; -- player
 local surface;
+
+--- ============ ---
+--- === UTIL === ---
+--- ============ ---
+
+function has_value(tab, val) -- check if key is in a table
+	for index, value in ipairs(tab) do
+		if value == val then return true end
+	end
+	return false
+end
+
+function get_dir(x, y) -- returns the direction from player position to {x. y}
+	local delta_x = x - p.position.x
+	local delta_y = y - p.position.y
+	if delta_x > 0.2 then
+		if     delta_y >  0.2 then return {walking = true, direction = defines.direction.southeast}
+		elseif delta_y < -0.2 then return {walking = true, direction = defines.direction.northeast}
+		else                       return {walking = true, direction = defines.direction.east}
+		end
+	elseif delta_x < -0.2 then
+		if     delta_y >  0.2 then return {walking = true, direction = defines.direction.southwest}
+		elseif delta_y < -0.2 then return {walking = true, direction = defines.direction.northwest}
+		else                       return {walking = true, direction = defines.direction.west}
+		end
+	else
+		if     delta_y >  0.2 then return {walking = true,  direction = defines.direction.south}
+		elseif delta_y < -0.2 then return {walking = true,  direction = defines.direction.north}
+		else                       return {walking = false, direction = defines.direction.north}
+		end
+	end
+end
 
 --- =================== ---
 --- ===  FUNCTIONS  === ---
@@ -43,6 +74,10 @@ function write_world()
 	game.print("world export done")
 end
 
+function craft(recipe, count)
+	rcon.print(p.begin_crafting{recipe=recipe, count=count})
+end
+
 --- ================== ---
 --- ===  COMMANDS  === ---
 --- ================== ---
@@ -70,6 +105,15 @@ commands.add_command("drawbox", nil, function(command)
 	mark_area(a.color, nil, a.x1, a.y1, a.x2, a.y2)
 end)
 
+commands.add_command("craft", nil, function(command)
+	local a = game.json_to_table(command.parameter)
+	if a == nil then -- check that the argument is valid
+		game.print("wrong input: " .. command.parameter)
+		return
+	end
+	craft(a.recipe, a.count)
+end)
+
 --- ================ ---
 --- ===  EVENTS  === ---
 --- ================ ---
@@ -81,6 +125,9 @@ script.on_event(defines.events.on_tick, function(event)
 	if walking_state.walking then
 		p.walking_state = walking_state
 		walking_state = get_dir(walking_to.x, walking_to.y)
-		if not walking_state.walking then game.print("walking done") end
+		if not walking_state.walking then
+			p.walking_state = walking_state -- stop the player
+			game.print("walking done")
+		end
 	end
 end)
